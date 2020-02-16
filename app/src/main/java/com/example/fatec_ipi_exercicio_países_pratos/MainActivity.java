0ba0fec62e06b8618704a25285dd9d1ff10730ae
Mayara Santos <mayaralimaasantos@hotmail.com>
2020-02-16 14:05:02 -0300

    Primeiro commit.

---
+++ app/src/main/java/com/example/fatec_ipi_exercicio_países_pratos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fatec_ipi_exercicio_países_pratos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
